b105ca00c2705c609bad53cbf200764e39bd1efa
Andreas Wilcox <UnknownBum@gmail.com>
2017-03-11 18:32:18 +0100

    Shows if server is up or down in admin interface

--- web_app/admin/view-model.js
+++ web_app/admin/view-model.js
@@ -8,6 +8,7 @@ function Tag(name) {
 bandcampMultiTag.controller('adminController', [ "$scope", "$http", function ($scope, $http) {
     $scope.adress = "localhost";
     $scope.port = 3000;
+    $scope.serverIsUp = false;
 
     var CreateAdminOptions = endpoint => { };
 
@@ -19,7 +20,9 @@ bandcampMultiTag.controller('adminController', [ "$scope", "$http", function ($s
         .then(response => {
             return response.data != "" ? JSON.parse(response.data) : "";
         })
-        .then(data => tile.body = data);
+        .then(data => tile.body = data)
+        .then(() => $scope.serverIsUp = true)
+        .catch(() => $scope.serverIsUp = false);
     };
 
     var cachedTagsFunc = function() {
@@ -34,8 +37,10 @@ bandcampMultiTag.controller('adminController', [ "$scope", "$http", function ($s
         callAdminService(this, "currentlycachingtag");
     }
 
+    var serverStatus = function() { this.body = $scope.serverIsUp ? "Online and ready =D" : "Server is down =(" };
+
     $scope.tiles = [
-        { header: "Server status", body: "Online and ready =D", update: () => {} },
+        { header: "Server status", body: "Online and ready =D", update: serverStatus },
         { header: "Cached tags", body: 0, update: cachedTagsFunc },
         { header: "Tags in queue", body: 0, update: tagsInQueue },
         { header: "Currently caching", body: "", update: currentlyCaching },
