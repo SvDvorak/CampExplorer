e15714f87e4655d48be3068881906d678363c953
Andreas Wilcox <UnknownBum@gmail.com>
2017-03-28 17:02:32 +0200

    Improved text-print in elastic search start script

--- server/elasticsearch/start.sh
+++ server/elasticsearch/start.sh
@@ -1,6 +1,3 @@
-echo "index setup"
-echo "what what!"
-
-# Create index if it doesn't exist
+printf "Starting elasticsearch\n"
 ./wait-for-it.sh localhost:9200 --timeout=60 --strict -- ./initial-index.sh &
 elasticsearch
