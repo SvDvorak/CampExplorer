b8eb495443eb0098b472f7de4f7f70250436dd9a
Andreas Wilcox <UnknownBum@gmail.com>
2017-04-07 10:53:16 +0200

    Removed a bunch of dead code now that cache has been replaced by a database

--- server/source/schedule-at.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = scheduleAt = function(date, func) {
-	var now = new Date();
-	var millisTillDate = date - now;
-	if (millisTillDate < 0) {
-	     millisTillDate += 86400000; // it's after specified time, try next time tomorrow.
-	}
-	setTimeout(func, millisTillDate);
-}
